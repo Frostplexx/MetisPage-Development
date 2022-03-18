6c3c066e711a4a7a3c3f6573f39cbc9994d12766
Daniel <daniel.inama02@gmail.com>
2022-03-18 10:16:31 +0100

    removed typescript

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,53 +1,58 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setToken = exports.authenticate = exports.playsong = exports.code = void 0;
-const topURL = "http://8184-2a02-810d-d40-65a0-d180-1fe0-37b0-dac8.ngrok.io";
-exports.code = "";
-function playsong(_url) {
-    const postURL = topURL + "/api/music/play";
-    //generate random id
-    const id = Math.floor(Math.random() * 1000000);
-    const body = {
-        "pairing_code": exports.code,
-        "songs": [
-            {
-                "song_id": id,
-                "url": _url,
-                "state": "playing",
-                "volume": "1",
-                "start_time": "0",
-            }
-        ]
-    };
-    runRequest("POST", body, postURL);
+const topURL = "http://8184-2a02-810d-d40-65a0-d180-1fe0-37b0-dac8.ngrok.io"
+export let code = "";
+
+export function playsong(_url){
+	const postURL = topURL + "/api/music/play"
+	//generate random id
+	const id = Math.floor(Math.random() * 1000000)
+	const body = {
+		"pairing_code": code,
+		"songs": [
+			{
+				"song_id": id,
+				"url": _url,
+				"state": "playing",
+				"volume": "1",
+				"start_time": "0",
+			}
+		]
+	}
+	runRequest("POST", body, postURL)
 }
-exports.playsong = playsong;
-function runRequest(type, data, url) {
-    return new Promise(function (resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.open(type, url);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(data));
-        xhr.onload = function () {
-            resolve(JSON.parse(xhr.response));
-        };
-        xhr.onerror = function () {
-            reject(xhr.statusText);
-        };
-    });
+
+function runRequest(type, data, url){
+	return new Promise(function (resolve, reject) {
+		const xhr = new XMLHttpRequest()
+		xhr.open(type, url)
+		xhr.setRequestHeader("Content-Type", "application/json")
+		xhr.send(JSON.stringify(data))
+		xhr.onload = function()  {
+			resolve(JSON.parse(xhr.response))
+		}
+		xhr.onerror = function() {
+			reject(xhr.statusText)
+		}
+	})
 }
+
 //check if token is valid
-async function authenticate(token) {
-    const __url = topURL + "/api/session/pair";
-    document.getElementById("btn-spinner").style.display = "inline-block";
-    const body = {
-        "pairing_code": token,
-    };
-    const response = await runRequest("POST", body, __url);
-    return response.status;
+export async function authenticate(token) {
+	const __url = topURL + "/api/session/pair";
+	document.getElementById("btn-spinner").style.display = "inline-block";
+	const body = {
+		"pairing_code": token,
+	}
+	const response = await runRequest("POST", body, __url);
+	return response.status
 }
-exports.authenticate = authenticate;
-function setToken(token) {
-    exports.code = token;
+
+export function setToken(token) {
+	code = token
 }
-exports.setToken = setToken;
+
+
+// interface APIResponse {
+// 	"status": boolean, // true if successful
+// 	"message": string, // message to display
+// 	"data": any | null // data to return
+// }
--- assets/js/music.js
+++ assets/js/music.js
@@ -1,22 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const api_1 = require("./api");
+import { authenticate, setToken } from "./api.js";
+
 async function login() {
-    const input = document.getElementById("sessionInput");
-    const password = input.value;
-    let loginwindow = document.getElementById("login");
-    let dashboardwindow = document.getElementById("dashboard");
-    let response = await (0, api_1.authenticate)(password);
-    if (response) {
-        loginwindow.style.display = "none";
-        document.getElementById("btn-spinner").style.display = "none";
-        dashboardwindow.style.display = "inline-block";
-        (0, api_1.setToken)(password);
-    }
-    else {
-        document.getElementById("btn-spinner").style.display = "none";
-        alert("Wrong Session ID");
-    }
+	const password = document.getElementById("sessionInput").value;
+	let loginwindow = document.getElementById("login");
+	let dashboardwindow = document.getElementById("dashboard");
+	let response = await authenticate(password);
+	if (response) {
+		loginwindow.style.display = "none";
+		document.getElementById("btn-spinner").style.display = "none";
+		dashboardwindow.style.display = "inline-block";
+		setToken(password);
+	} else {
+		document.getElementById("btn-spinner").style.display = "none";
+		alert("Wrong Session ID");
+	}
 }
-const loginButton = document.getElementById("loginButton");
+
+const loginButton = document.getElementById("submitbutton");
 loginButton.addEventListener("click", login);
