c779128df5934f767865976469cd92bcf90491b4
Daniel <daniel.inama02@gmail.com>
2022-03-18 17:55:57 +0100

    updated music player look

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 const topURL = "http://8184-2a02-810d-d40-65a0-d180-1fe0-37b0-dac8.ngrok.io"
 export let code = "";
 
-export function playsong(_url){
+export function playMusic(_url) {
 	const postURL = topURL + "/api/music/play"
 	//generate random id
 	const id = Math.floor(Math.random() * 1000000)
@@ -18,22 +18,22 @@ export function playsong(_url){
 		]
 	}
 	runRequest("POST", body, postURL)
+	return id;
 }
 
-function runRequest(type, data, url){
-	return new Promise(function (resolve, reject) {
-		const xhr = new XMLHttpRequest()
-		xhr.open(type, url)
-		xhr.setRequestHeader("Content-Type", "application/json")
-		xhr.send(JSON.stringify(data))
-		xhr.onload = function()  {
-			resolve(JSON.parse(xhr.response))
+//pause music
+export function pauseMusic(songid, _url) {
+	const postURL = topURL + "/api/music/update/" + songid
+	const body = {
+		"pairing_code": code,
+		"song": {
+			"song_id": songid,
+			"state": "paused",
 		}
-		xhr.onerror = function() {
-			reject(xhr.statusText)
-		}
-	})
+	}
+	runRequest("PUT", body, postURL)
 }
+
 
 //check if token is valid
 export async function authenticate(token) {
@@ -50,9 +50,17 @@ export function setToken(token) {
 	code = token
 }
 
-
-// interface APIResponse {
-// 	"status": boolean, // true if successful
-// 	"message": string, // message to display
-// 	"data": any | null // data to return
-// }
+function runRequest(type, data, url) {
+	return new Promise(function (resolve, reject) {
+		const xhr = new XMLHttpRequest()
+		xhr.open(type, url)
+		xhr.setRequestHeader("Content-Type", "application/json")
+		xhr.send(JSON.stringify(data))
+		xhr.onload = function () {
+			resolve(JSON.parse(xhr.response))
+		}
+		xhr.onerror = function () {
+			reject(xhr.statusText)
+		}
+	})
+}
--- assets/js/music.js
+++ assets/js/music.js
@@ -1,4 +1,9 @@
-import { authenticate, setToken } from "./api.js";
+import { authenticate, setToken, playMusic, pauseMusic } from "./api.js";
+
+
+//authentication handling -- checks if token is valid
+const loginButton = document.getElementById("submitbutton");
+loginButton.addEventListener("click", login);
 
 async function login() {
 	const password = document.getElementById("sessionInput").value;
@@ -16,5 +21,98 @@ async function login() {
 	}
 }
 
-const loginButton = document.getElementById("submitbutton");
-loginButton.addEventListener("click", login);
+// button event handling
+// const wrapper = document.getElementById("song-list-wrapper")
+// $('#song-list-wrapper a').addEventListener("click", (btnEvent) => mscBtnHandler(btnEvent));
+$('#song-list-wrapper a').on("click", (btnEvent) => mscBtnHandler(btnEvent));
+
+function mscBtnHandler(btnCLickEvent){
+	//get the button that was clicked
+	let btn = btnCLickEvent.target;
+	//get the classes of the parent as a array
+	let classes = btn.classList;
+	//check if the parent has one of the event classes
+	console.log(classes);
+	if (classes.contains("play")) {
+		//play the song
+		playSong(btn);
+	}
+	if (classes.contains("pause")) {
+		//pause the song
+		pauseSong(btn);
+	}
+	if (classes.contains("restart")) {
+		//restart the song
+		restartSong(btn);
+	}
+	if (classes.contains("edit")) {
+		//edit the song
+		editSong(btn);
+	}
+	if (classes.contains("settings")) {
+		//edit the song
+		settingsSong(btn);
+	}
+}
+
+
+function playSong(btn){
+	//get the song url
+	let url = btn.getElementsByClassName("song-url")[0].innerHTML;
+
+	console.log("URL: " + url);
+	let songId = playMusic(url);
+	//get the children of the parent
+	delay(100).then(() => {changeButtonState(btn, "play", "pause");});
+	//add song id to the parent
+	document.getElementById("songTitle").innerText = btn.innerText;
+	document.getElementById("cover").src = parseYoutubeThumbnail(url);
+	console.log(btn.innerText)
+	btn.setAttribute("song-id", songId);
+	changePlayButtonState(true);
+}
+
+
+
+function pauseSong(parent){
+	let parent1 = parent.parentNode.parentNode;
+	let id = parent1.getAttribute("song-id");
+
+	let url = document.getElementById(parent1.id).getElementsByClassName("song-url")[0].innerHTML;
+	//get the song id of the parent
+	pauseMusic(id, url);
+	changeButtonState(parent, "pause", "play");
+	let children = parent.children;
+	console.log(children)
+	children[0].classList.remove("fa-pause");
+	children[0].classList.add("fa-play");
+	console.log("Running pauseSong");
+}
+
+
+function changePlayButtonState(state){
+	if(state){
+		$('#btn-play em').removeClass("fa-play");
+		$('#btn-play em').addClass("fa-pause");
+	} else if (!state){
+		$('#btn-play em').removeClass("fa-pause");
+		$('#btn-play em').addClass("fa-play");
+	}
+}
+
+
+function changeButtonState(button, prevState, newState){
+	button.classList.remove(prevState);
+	button.classList.add(newState);
+}
+
+function delay(time) {
+	return new Promise(resolve => setTimeout(resolve, time));
+  }
+  
+
+function parseYoutubeThumbnail(url){
+	url.split("=");
+	let id = url.split("=")[1];
+	return "https://img.youtube.com/vi/" + id + "/0.jpg";
+}
