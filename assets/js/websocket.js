ab7b58314bdb95af5f066d1cb34799a94e26344e
Daniel <daniel.inama02@gmail.com>
2022-04-06 10:23:23 +0200

    update server url

--- assets/js/websocket.js
+++ assets/js/websocket.js
@@ -4,7 +4,7 @@ export let code = "";
 var serverURL = localStorage.getItem("serverURL");
 if(serverURL != null) serverURL.replace("tcp://", "wss://") 
 if (serverURL == null || serverURL === "" || serverURL === undefined || serverURL.includes("ws://")) {
-	serverURL = "wss://preview.webhook.metisbot.xyz";
+	serverURL = "wss://preview.webhook.metisbot.xyz/ws";
 }
 //------- setting up ping-pong mechanism --------//
 console.log("Connecting to " + serverURL);
