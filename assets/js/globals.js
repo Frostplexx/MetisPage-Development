2f698099287211dc647a134dc11f6af26d737fcf
Frostplexx <daniel.inama02@gmail.com>
2022-09-14 15:09:51 +0200

    fixed url

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -1,3 +1,3 @@
 export const bearerToken = "M2FLX3UrKyZzTkFMayQrWkt2NFE="; 
-// export const baseUrl = "https://preview.webhook.metisbot.xyz";
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://preview.webhook.metisbot.xyz";
+// export const baseUrl = "http://localhost:3000";
