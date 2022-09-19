d2784765713786d9c0245992d3343658d017fc46
Frostplexx <daniel.inama02@gmail.com>
2022-09-19 17:40:32 +0200

    reformat for better legibitlity

--- assets/js/editCampaign.js
+++ assets/js/editCampaign.js
@@ -177,7 +177,7 @@ function parseHTMLFormToGoogleFormData(form) {
 		location: formData.get("location"),
 		time: formData.get("time"),
 		notes: formData.get("notes"),
-		new_dm_tag: formData.get("new_dm_tag"),
+		new_dm_tag: formData.get("new_dm_tag") ? formData.get("new_dm_tag") : "",
 	};
 }
 
