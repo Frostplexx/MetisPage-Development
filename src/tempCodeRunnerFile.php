9f1322a696d5f1df9deed85905eeb5f8538147f6
Daniel <daniel.inama02@gmail.com>
2022-11-17 21:35:11 +0100

    new login page design

---
+++ src/tempCodeRunnerFile.php
@@ -0,0 +1,6 @@
+<?php
+// load header, main page and footer
+$header = fopen("../assets/oldHTML/header.html", "r");
+$footer = fopen("../assets/oldHTML/footer.html", "r");
+
+echo fread($header, filesize("../assets/oldHTML/header.html"));
